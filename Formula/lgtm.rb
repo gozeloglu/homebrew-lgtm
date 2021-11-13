0a68fd3c737eb6bad9bb65ba0b6414b590a4153f
Gökhan Özeloğlu <gozeloglu@gmail.com>
2021-11-13 17:09:58 +0300

    Fix url
    License added.
    
    Signed-off-by: Gökhan Özeloğlu <gozeloglu@gmail.com>

--- Formula/lgtm.rb
+++ Formula/lgtm.rb
@@ -1,8 +1,9 @@
 class lgtm < Formula
 	desc "A simple tool for copying LGTM fancy text to your clipboard.
 	homepage "https://github.com/gozeloglu/lgtm"
-	url "https://github.com/gozeloglu/lgtm/releasess/latest/download/lgtm-mac.tar.gz"
+	url "https://github.com/gozeloglu/lgtm/releases/latest/download/lgtm-mac.tar.gz"
 	sha256 "34542797b1e30695601fef0f530d4ef208d36985e323615c6c501a844a917932"
+	license "MIT"
 	version "0.3.0"
 
 	def install
