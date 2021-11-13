f5348c32f2673c8c10a0f4428de2db3821e3e91a
Gökhan Özeloğlu <gozeloglu@gmail.com>
2021-11-13 20:30:25 +0300

    Fix syntax error
    Signed-off-by: Gökhan Özeloğlu <gozeloglu@gmail.com>

--- Formula/lgtm.rb
+++ Formula/lgtm.rb
@@ -1,7 +1,6 @@
-require "UTF-8"
 class Lgtm < Formula
-	desc "A simple tool for copying LGTM fancy text to your clipboard.
-	homepage "https://github.com/gozeloglu/lgtm"
+	desc "A simple tool for copying LGTM fancy text to your clipboard."
+	homepage "https://github.com/gozeloglu/lgtm/"
 	url "https://github.com/gozeloglu/lgtm/releases/latest/download/lgtm-mac.tar.gz"
 	sha256 "34542797b1e30695601fef0f530d4ef208d36985e323615c6c501a844a917932"
 	license "MIT"
